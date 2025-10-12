caa3293dd13d655e5047724995f002f026d25c98
jonBeach <jonb3ach@gmail.com>
2025-10-11 23:39:17 -0400

    Added exact copy of twitch oauth to be turned into twitch oauth code grant flow for getting a refresh token so the user doesnt have to reauthorize after uncertain amount of time.

---
+++ twitch_oauth_code_grant_flow.py
@@ -0,0 +1,178 @@
+import webbrowser
+import os
+import requests
+import json
+from env_manager import Env
+
+# Load env variables
+env = Env()
+env.load_env()
+CLIENT_ID = env.get_env_variable('CLIENT_ID')
+REDIRECT_URI = env.get_env_variable('REDIRECT_URI')
+
+# Scope of what the oauth wants to access
+SCOPES = [
+	'user:read:follows',
+	'moderator:read:followers'
+]
+
+# oauth url
+auth_url = (
+	f'https://id.twitch.tv/oauth2/authorize'
+	f'?response_type=token'
+	f'&client_id={CLIENT_ID}'
+	f'&redirect_uri={REDIRECT_URI}'
+	f'&scope={" ".join(SCOPES)}'
+)
+
+defualt_browser = True
+
+# Some browser paths for webbrowser registration
+browser_paths = [
+	('chrome','C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'),
+	('chrome','C:/Program Files/Google/Chrome/Application/chrome.exe'),
+	('firefox','C:/Program Files/Mozilla Firefox/firefox.exe'),
+	('firefox','C:/Program Files (x86)/Mozilla Firefox/firefox.exe')
+]
+
+
+def register_browsers() -> None:
+	"""
+	Registers browsers so webbrowser can find them
+	without this webbrowser cant see any browsers
+	"""
+	for name, path in browser_paths:
+		if os.path.isfile(path):
+			webbrowser.register(name, None, webbrowser.BackgroundBrowser(path))
+
+
+def get_browser(browser: str) -> webbrowser.BackgroundBrowser | bool:
+	"""Changes browser if found"""
+	try:
+		browser = webbrowser.get(browser)
+		return browser
+	except webbrowser.Error:
+		print('Failed to find browser. Using default browser!')
+		return None
+
+
+def get_user_id(access_token: str) -> str | None:
+
+	user_id_url = 'https://api.twitch.tv/helix/users'
+	headers = {
+		'Authorization': f'Bearer {access_token}',
+		'Client-Id': CLIENT_ID
+	}
+
+	response = requests.get(user_id_url, headers=headers)
+
+	if response.status_code == 200:
+		user_id = response.json()['data'][0]['id']
+		return user_id
+	else:
+		print(
+			f'Error getting user id!\n'
+			f'Staus Code: {response.status_code}\n'
+			f'Error Message: {response.text}'
+		)
+		return None
+
+
+def edit_html_file(access_token: str, user_id: str) -> bool:
+	html_injection_text = [
+		"\t<script id='config' type='application/json'>\n",
+		"\t\t{\n",
+		f'\t\t\t"access_token": "{access_token}",\n',
+		f'\t\t\t"client_id": "{CLIENT_ID}",\n',
+		f'\t\t\t"user_id": "{user_id}"\n',
+		"\t\t}\n",
+		"\t</script>\n"
+	]
+	try:
+		with open('defaultindex', 'r') as html_file:
+			lines = html_file.readlines()
+
+	except Exception as e:
+		print(f'Error reading defaultindex file')
+		return False
+
+	lines = lines[:lines.index('<body>\n')+1] + html_injection_text + lines[lines.index('<body>\n')+1:]
+
+	try:
+		with open('index.html', 'w') as new_html:
+			new_html.writelines(lines)
+	except Exception as e:
+		print(f'Error writing to index.html file')
+		return False
+
+	print('index.html successfully injected!')
+	return True
+
+def main():
+	register_browsers()
+
+	print(
+		'OAuth for twitch account needed!\n'
+		'Web browser will be used to log you in.\n'
+	)
+
+	print(
+		'Enter nothing to use the default browser\n'
+		'If you would like to use non default browser'
+		' - enter your browser of choice!\n'
+		'Common browser names:'
+	)
+
+	browser_names = [
+		'chrome',
+		'firefox'
+	]
+
+	# Formating in console
+	for browser in browser_names:
+		print(f'{" ":>2}- {browser}')
+
+	change_browser = input('\nBrowser: ')
+
+
+	print(
+		'Opening browser!\n'
+		'When redirected to "localhost" paste the entire url in here'
+	)
+	if change_browser:
+		new_browser = get_browser(change_browser)
+		if new_browser:
+			new_browser.open(auth_url)
+		else:
+			webbrowser.open(auth_url)
+	else:
+		webbrowser.open(auth_url)
+
+	access_token = input('\nOauth URL: ')
+
+	access_token = access_token[18:]
+
+	access_token = access_token.split('&')
+
+	# Get access token from url
+	for i in access_token:
+		i = i.split('=')
+		if i[0] == 'access_token':
+			access_token = i[1]
+			break
+
+	try:
+		user_id = get_user_id(access_token)
+		html_success = edit_html_file(access_token, user_id)
+		print(f'\nAccess Token: {access_token}')
+
+		if html_success:
+			print('Everything is setup, just run "index.html" !')
+	except Exception as e:
+		print(
+			f'This might be your access token: {access_token}'
+		)
+
+
+if __name__ == '__main__':
+	main()
